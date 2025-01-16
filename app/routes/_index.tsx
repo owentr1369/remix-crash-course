0662fecd8fb13376464b33b361a23620eee329e5
TranTam <tamvaa1306@gmail.com>
2025-01-16 15:16:41 +0700

    update login header to reflect application name

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -57,7 +57,7 @@ export default function Index() {
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-2xl font-bold mb-6 text-center text-gray-700">
-          Login
+          Remix App Simple Login
         </h1>
         <Form method="post" className="space-y-6">
           <div>
