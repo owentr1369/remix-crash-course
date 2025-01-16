eacae3974efaa1b5ac9948d4706714a65035e259
TranTam <tamvaa1306@gmail.com>
2025-01-16 15:12:01 +0700

    add profile actions for user logout and account deletion

--- app/routes/profile.$id.tsx
+++ app/routes/profile.$id.tsx
@@ -1,5 +1,5 @@
-import { redirect, useLoaderData } from "@remix-run/react";
-import { findUser, IUser } from "data/user";
+import { redirect, useLoaderData, Form } from "@remix-run/react";
+import { deleteUser, findUser, IUser } from "data/user";
 
 export const loader = async ({ params }: { params: { id: string } }) => {
   const currentUser = findUser(params.id);
@@ -13,8 +13,32 @@ export const loader = async ({ params }: { params: { id: string } }) => {
   });
 };
 
+export const action = async ({
+  params,
+  request,
+}: {
+  params: { id: string };
+  request: Request;
+}) => {
+  const formData = await request.formData();
+  console.log(formData);
+  const actionType = formData.get("action") as string;
+  if (actionType === "delete") {
+    deleteUser(params.id);
+    return redirect("/");
+  } else if (actionType === "logout") {
+    return redirect("/");
+  }
+};
+
 const Profile = () => {
   const user = useLoaderData<IUser>();
+  const handleClientSideLogout = async (action: string) => {
+    if (action === "logout" || action === "delete") {
+      console.log(action);
+      localStorage.removeItem("user");
+    }
+  };
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
@@ -32,6 +56,32 @@ const Profile = () => {
               <p className="text-sm font-medium text-gray-500">Username</p>
               <p className="text-gray-700">{user.name}</p>
             </div>
+          </div>
+          <div className="flex space-x-4 mt-6">
+            <Form
+              method="post"
+              onSubmit={() => handleClientSideLogout("logout")}
+            >
+              <input type="hidden" name="action" value="logout" />
+              <button
+                type="submit"
+                className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600"
+              >
+                Logout
+              </button>
+            </Form>
+            <Form
+              method="post"
+              onSubmit={() => handleClientSideLogout("delete")}
+            >
+              <input type="hidden" name="action" value="delete" />
+              <button
+                type="submit"
+                className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600"
+              >
+                Delete Account
+              </button>
+            </Form>
           </div>
         </div>
       </div>
